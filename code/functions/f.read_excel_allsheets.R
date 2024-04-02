a6e2d5acfe0e6f8b1c2967273f9f80a4c6755467
spbruneel-INBO <stijn.bruneel@inbo.be>
2024-04-02 16:12:07 +0200

    update read excel function

--- code/functions/f.read_excel_allsheets.R
+++ code/functions/f.read_excel_allsheets.R
@@ -1,6 +1,6 @@
-f.read_excel_allsheets <- function(filename, tibble = FALSE) {
+f.read_excel_allsheets <- function(filename, tibble = FALSE, skip=0, col_names=FALSE) {
   sheets <- readxl::excel_sheets(filename)
-  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X, col_names=col_names, skip=skip))
   if(!tibble) x <- lapply(x, as.data.frame)
   names(x) <- sheets
   x
