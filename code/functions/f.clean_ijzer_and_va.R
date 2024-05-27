09de6209377c07d0bd66872685b662d95ba37370
spbruneel-INBO <stijn.bruneel@inbo.be>
2024-05-27 09:31:01 +0200

    make function more generic by using "data" instead of "Ijzer"

--- code/functions/f.clean_ijzer_and_va.R
+++ code/functions/f.clean_ijzer_and_va.R
@@ -1,45 +1,45 @@
-clean_ijzer_and_va<-function(file,skip,datum,tijd,opmerking,site){
-  Ijzer<-f.read_excel_allsheets(file,skip=skip)
-  Ijzer<-rbindlist(Ijzer)
+clean_data_and_va<-function(file,skip,datum,tijd,opmerking,site){
+  data<-f.read_excel_allsheets(file,skip=skip)
+  data<-rbindlist(data)
   variable.select<-c(datum,tijd,opmerking)
-  Ijzer<-Ijzer[,..variable.select]
-  Ijzer<-Ijzer[rowSums(is.na(Ijzer)) != ncol(Ijzer), ]
-  colnames(Ijzer)<-c("datum","tijd","opmerking")
-  Ijzer$datum<-zoo::na.locf(Ijzer$datum)
-  Ijzer$tijd <- format(Ijzer$tijd,"%H:%M:%S")
-  Ijzer$datum <- format(Ijzer$datum,"%y-%m-%d")
-  Ijzer$tijd <- parse_date_time(paste(Ijzer$datum,Ijzer$tijd), c("ymd HMS"))
-  Ijzer$datum<-NULL
-  Ijzer$opmerking<-tolower((Ijzer$opmerking))
-  Ijzer$opmerking[which(Ijzer$opmerking %in% c("omgekeerd spuien",
+  data<-data[,..variable.select]
+  data<-data[rowSums(is.na(data)) != ncol(data), ]
+  colnames(data)<-c("datum","tijd","opmerking")
+  data$datum<-zoo::na.locf(data$datum)
+  data$tijd <- format(data$tijd,"%H:%M:%S")
+  data$datum <- format(data$datum,"%y-%m-%d")
+  data$tijd <- parse_date_time(paste(data$datum,data$tijd), c("ymd HMS"))
+  data$datum<-NULL
+  data$opmerking<-tolower((data$opmerking))
+  data$opmerking[which(data$opmerking %in% c("omgekeerd spuien",
                                                "omgekeerd spuibeheer",
                                                "omgekeerd spuibeheer opgestart in opdracht van tim wolff",
                                                "en start omgekeerd spuibeheer"))]="open"
-  Ijzer$opmerking[which(Ijzer$opmerking %in% c("einde omgekeerd spuien",
+  data$opmerking[which(data$opmerking %in% c("einde omgekeerd spuien",
                                                "einde os iov dré maes",
                                                "einde omgekeerd spuibeheer",
                                                "einde omgekeerde spuibeheer"))]="dicht"
-  Ijzer$opmerking[(which(!Ijzer$opmerking %in% c("open","dicht")))]=NA
-  #Ijzer <- Ijzer[-which((format(Ijzer$tijd,"%H:%M:%S") %in% c("08:00:00","18:00:00")) & (is.na(Ijzer$opmerking)==TRUE)),]
+  data$opmerking[(which(!data$opmerking %in% c("open","dicht")))]=NA
+  #data <- data[-which((format(data$tijd,"%H:%M:%S") %in% c("08:00:00","18:00:00")) & (is.na(data$opmerking)==TRUE)),]
   
-  Ijzer$opmerking.lag<-dplyr::lag(Ijzer$opmerking)
-  Ijzer$opmerking.lead<-lead(Ijzer$opmerking)
-  Ijzer$opmerking.lag<-zoo::na.locf(Ijzer$opmerking.lag,na.rm=FALSE,fromLast = TRUE)
-  Ijzer$opmerking.lead<-zoo::na.locf(Ijzer$opmerking.lead,na.rm=FALSE,fromLast = TRUE)
-  Ijzer$opmerking[which(Ijzer$opmerking=="open" & Ijzer$opmerking.lag=="open" & Ijzer$opmerking.lead=="open")+1]="dicht"
+  data$opmerking.lag<-dplyr::lag(data$opmerking)
+  data$opmerking.lead<-lead(data$opmerking)
+  data$opmerking.lag<-zoo::na.locf(data$opmerking.lag,na.rm=FALSE,fromLast = TRUE)
+  data$opmerking.lead<-zoo::na.locf(data$opmerking.lead,na.rm=FALSE,fromLast = TRUE)
+  data$opmerking[which(data$opmerking=="open" & data$opmerking.lag=="open" & data$opmerking.lead=="open")+1]="dicht"
   
-  Ijzer$opmerking.lag<-dplyr::lag(Ijzer$opmerking)
-  Ijzer$opmerking.lead<-lead(Ijzer$opmerking)
-  Ijzer$opmerking.lag<-zoo::na.locf(Ijzer$opmerking.lag,na.rm=FALSE,fromLast = FALSE)
-  Ijzer$opmerking.lead<-zoo::na.locf(Ijzer$opmerking.lead,na.rm=FALSE,fromLast = FALSE)
-  Ijzer$opmerking[which(Ijzer$opmerking=="dicht" & Ijzer$opmerking.lag=="dicht" & Ijzer$opmerking.lead=="dicht")-1]="open"
+  data$opmerking.lag<-dplyr::lag(data$opmerking)
+  data$opmerking.lead<-lead(data$opmerking)
+  data$opmerking.lag<-zoo::na.locf(data$opmerking.lag,na.rm=FALSE,fromLast = FALSE)
+  data$opmerking.lead<-zoo::na.locf(data$opmerking.lead,na.rm=FALSE,fromLast = FALSE)
+  data$opmerking[which(data$opmerking=="dicht" & data$opmerking.lag=="dicht" & data$opmerking.lead=="dicht")-1]="open"
   
-  Ijzer<-Ijzer[-which(is.na(Ijzer$opmerking)==TRUE),c("tijd","opmerking")]
-  Ijzer <- Ijzer %>%
+  data<-data[-which(is.na(data$opmerking)==TRUE),c("tijd","opmerking")]
+  data <- data %>%
     group_by(opmerking) %>%
     mutate(row = row_number()) %>%
     tidyr::pivot_wider(names_from = opmerking, values_from = tijd) %>%
     dplyr::select(-row)
-  Ijzer$site=site
-  return(Ijzer)
+  data$site=site
+  return(data)
 }
