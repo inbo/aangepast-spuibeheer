423d1ca6c323e04b5c86f8b10ed2645e38dd4c24
spbruneel-INBO <stijn.bruneel@inbo.be>
2024-06-03 11:24:44 +0200

    cleaning of kgo in function

---
+++ code/functions/f.clean_KGO.R
@@ -0,0 +1,11 @@
+clean_KGO <- function(file){
+  data <- read_excel(file)
+  data$open[which(grepl("[0-9]", data$open) == FALSE)] = NA
+  data$dicht[which(grepl("[0-9]", data$dicht) == FALSE)] = NA
+  data <- data[-which(is.na(data$open) == TRUE | is.na(data$dicht) == TRUE),]
+  data$open <- parse_date_time(paste(data$datum,data$open), c("ymd HM", "ymd H"))
+  data$dicht <- parse_date_time(paste(data$datum,data$dicht), c("ymd HM", "ymd H"))
+  data$datum <- NULL
+  data$site = "KGO"
+  return(data)
+}
