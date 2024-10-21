091cba82170660a8d806c83a28f7ffb2ea9d3796
spbruneel-INBO <stijn.bruneel@inbo.be>
2024-10-21 17:12:40 +0200

    raw in brussels; processed into gmt; remove duplicates; fill with site; join with neerslag

--- code/not_functions/combine_ctd.R
+++ code/not_functions/combine_ctd.R
@@ -20,14 +20,14 @@ for (i in jaren){
             ctd.temp[,2]<-ctd.temp[,2]/1000
           }
           colnames(ctd.temp)<-c("datum.ctd","geleidbaarheid","temperatuur")
-          ctd.temp$datum.ctd<-parse_date_time(ctd.temp$datum.ctd, c("mdy HMS p"))
+          ctd.temp$datum.ctd<-parse_date_time(ctd.temp$datum.ctd, c("mdy HMS p"), tz="Europe/Brussels")
           ctd.temp$sensor<-"small header"
         } else{
           ctd.temp<-read.csv(dir.name4,skip=65,sep=";",header=FALSE)
           ctd.temp<-ctd.temp[-nrow(ctd.temp),]
           colnames(ctd.temp)<-c("datum.ctd","druk","temperatuur","geleidbaarheid")
           ctd.temp<-as.data.frame(lapply(ctd.temp, function(y) gsub(",", ".", y)))
-          ctd.temp$datum.ctd<-parse_date_time(ctd.temp$datum.ctd, c("ymd HMS"))
+          ctd.temp$datum.ctd<-parse_date_time(ctd.temp$datum.ctd, c("ymd HMS"), tz="Europe/Brussels")
           ctd.temp$sensor<-"large header"
         }
         ctd.temp$site<-j
@@ -40,7 +40,9 @@ for (i in jaren){
 }
 remove(ctd.temp)
 
-ctd$datum.ctd<-as.POSIXct(ctd$datum.ctd,format='%Y-%m-%d %H:%M:%S')
+ctd$datum.ctd<-as.POSIXct(ctd$datum.ctd,format='%Y-%m-%d %H:%M:%S',tz="Europe/Brussels")
+attr(ctd$datum.ctd, "tzone") <- "GMT"
+
 ctd[c("druk","temperatuur","geleidbaarheid")] <- sapply(ctd[c("druk","temperatuur","geleidbaarheid")],as.numeric)
 ctd <- ctd %>% distinct()
 ctd <- ctd %>% mutate(loc.ctd= case_when(str_detect(filename, 'akl ramskapelle|akl rampskapelle') ~ 'akl ramskapelle',
@@ -66,12 +68,12 @@ ctd <- ctd %>% mutate(loc.ctd= case_when(str_detect(filename, 'akl ramskapelle|a
 source("./code/functions/f.seq_date.R")
 source("./code/functions/f.grouping_breaking_line.R")
 ctd$datum.ctd<-round_date(ctd$datum.ctd, unit="30 mins")
-x <- do.call("rbind", by(ctd, ctd$loc.ctd, with, data.frame(loc.ctd = loc.ctd[1], datum.ctd = seq_date(datum.ctd))))
+x <- do.call("rbind", by(ctd, ctd$loc.ctd, with, data.frame(loc.ctd = loc.ctd[1], datum.ctd = seq_date(datum.ctd,"30 mins"))))
 ctd <- left_join(x,ctd,by=c("loc.ctd","datum.ctd"))
 ctd$group_plot <- as.character(grouping_breaking_line(ctd$filename))
+x <- ctd %>% group_by(loc.ctd) %>% fill(site)
 
 link_ctd_debiet_neerslag<-read.csv("./data/link_ctd_debiet_neerslag.csv",sep=";")
 ctd<-left_join(ctd,link_ctd_debiet_neerslag,by="loc.ctd")
-ctd <- ctd[which(is.na(ctd$datum.ctd)==FALSE),]
 ctd <- ctd %>% left_join(debiet, join_by(loc.debiet, closest(datum.ctd >= datum.debiet)))
 ctd <- ctd %>% left_join(neerslag, join_by(loc.neerslag, closest(datum.ctd >= datum.neerslag)))
