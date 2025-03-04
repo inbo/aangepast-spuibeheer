16fb9942eb7aff8e11a91523f5cc43085226ff04
spbruneel-INBO <stijn.bruneel@inbo.be>
2025-03-04 14:40:45 +0100

    new not_function to combine everything to keep results rmd clean

---
+++ code/not_functions/combine_all.R
@@ -0,0 +1,9 @@
+source("./code/not_functions/libraries.R")
+source("./code/functions/f.read_excel_allsheets.R")
+
+source("./code/functions/f.clean_ijzer_and_va.R")
+source("./code/functions/f.clean_KGO.R")
+source("./code/functions/f.clean_AKLLK.R")
+source("./code/not_functions/combine_as_NE.R")
+source("./code/not_functions/combine_as_data.R")
+
