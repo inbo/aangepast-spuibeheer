dcec192e710a7d1cf363ca03a63850950606718a
spbruneel-INBO <stijn.bruneel@inbo.be>
2025-03-05 16:47:35 +0100

    switch x and y

--- code/not_functions/coordinaten_ctd.R
+++ code/not_functions/coordinaten_ctd.R
@@ -4,4 +4,4 @@ file<-read.csv("./data/metadata/coordinaten_ctd.csv",sep=";")
 file[c('X', 'Y')] <- as.numeric(str_split_fixed(file$X..Y, ',', 2))
 file$X..Y<-NULL
 map<-f.map(file,"Y","X",crs="+init=epsg:4326","site","loc")
-f.map(file,"X","Y",crs="+init=epsg:4326","site","loc",plot=TRUE)
+f.map(file,"Y","X",crs="+init=epsg:4326","site","loc",plot=TRUE)
