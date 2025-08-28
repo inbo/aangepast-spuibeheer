9bfe11b9f42516be20ae92a405bee91fd60767f1
spbruneel-INBO <stijn.bruneel@inbo.be>
2025-08-28 11:28:33 +0200

    add references for report of 2022

---
+++ code/not_functions/add.refs.2022.R
@@ -0,0 +1,10 @@
+INBOmd::add_report_numbers(
+  path="./",
+  doi="10.21436/inbor.132570110",
+  year=2025,
+  reportnr=50,
+  depotnr="D/2025/3241/307",
+  photo="./media/DSCN2815.JPG",
+  description="Optrekkende glasaal die het Kanaal Gent-oostende kan opzwemmen door het aangepast spuibeheer. Foto: David Buysse (INBO)")
+
+INBOmd::validate_doi("10.21436/inbor.132570110")
